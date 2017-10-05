fcdfdd270b84f4d50bde1d8b3e0215cd9b872c3f
Kevin Yeh <revinyeh@gmail.com>
2017-10-04 17:50:44 -0700

    changed home page a little bit

--- react-client/src/components/SendRequestView.jsx
+++ react-client/src/components/SendRequestView.jsx
@@ -1,15 +1,25 @@
 import React from 'react';
+// import DatePicker from 'react-date-picker';
 
 class SendRequest extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      date: new Date(),
+    };
+  }
+
+
+
   render() {
     return (
       <div>
-        <button type="button" className="btn btn-primary" data-toggle="modal" data-target="#exampleModal" data-whatever="@getbootstrap">Sitting Request Form</button>
+        <button type="button" className="btn btn-primary" data-toggle="modal" data-target="#exampleModal" data-whatever="@getbootstrap">Contact sitter</button>
         <div className="modal fade" id="exampleModal" tabIndex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-          <div className="modal-dialog modal-lg" role="document">
+          <div className="modal-dialog" role="document">
             <div className="modal-content">
               <div className="modal-header">
-                <h2 className="modal-title" id="exampleModalLabel">Sitter Information</h2>
+                <h3 className="modal-title" id="exampleModalLabel">You are sending a reservation request to</h3>
                 <button type="button" className="close" data-dismiss="modal" aria-label="Close">
                   <span aria-hidden="true">&times;</span>
                 </button>
@@ -29,9 +39,10 @@ class SendRequest extends React.Component {
                   </h5>
                   <small>I am a cat lover!!</small>
                 </div>
+
                 <form>
                   <div className="form-group">
-                    <label htmlFor="message-text" className="form-control-label">Request Detial Message:</label>
+                    <label htmlFor="message-text" className="form-control-label">Request Detail Message:</label>
                     <textarea className="form-control" id="message-text"></textarea>
                   </div>
                 </form>
--- react-client/src/components/SitterInfoView.jsx
+++ react-client/src/components/SitterInfoView.jsx
@@ -15,29 +15,25 @@ class SittersInfo extends React.Component {
   render() {
     return (
       <div>
-        <div className="container">
-          <div className="row">
-            <div onClick={this.handleClick} className="col-xs-12">
-              SitterInfo
-              <div>{this.props.data.zipcode}</div>
-              <div>{this.props.data.description}</div>
-              <div className='modal' id='loginModal' tabIndex='-1'>
-                <div className='modal-dialog'>
-                  <div className='modal-content'>
-                    <div className='modal-header'>
-                      <button className="lose" data-dismiss='modal'>&times;</button>
-                      <h4 className='modal-title'>Totle</h4>
-                      <div className='modal-body'>
-                        body
-                      </div>
-                    </div>
-                  </div>
-                </div>
+        <div className="sitter-info">
+          <div className="container">
+            <div className="row">
+              <div onClick={this.handleClick} className="col-lg-12">
+                <h3>Sitter Infomation</h3>
+                <div>ZIP Code: {this.props.data.zipcode}</div>
+                <div>description: {this.props.data.description}</div>
+                <div>Review</div>
               </div>
             </div>
           </div>
         </div>
-        <SendRequest />
+        <div className="row">
+          <div className="col-lg-8">
+          </div>
+          <div className="col-lg-4">
+            <SendRequest />
+          </div>
+        </div>
       </div>
     );
   }

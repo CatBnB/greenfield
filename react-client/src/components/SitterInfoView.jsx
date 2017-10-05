3d4e28dc6df02bc2a6eec9613339b03c81998d6c
Kevin Yeh <revinyeh@gmail.com>
2017-10-04 21:44:32 -0700

    Sitter Infomation& Sitter View & Sitter Request form pages all done

--- react-client/src/components/SitterInfoView.jsx
+++ react-client/src/components/SitterInfoView.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import SendRequest from './SendRequestView.jsx';
+import SitterInfoViewReview from './SitterInfoViewReview.jsx'
 
 class SittersInfo extends React.Component {
   constructor(props) {
@@ -20,9 +21,51 @@ class SittersInfo extends React.Component {
             <div className="row">
               <div onClick={this.handleClick} className="col-lg-12">
                 <h3>Sitter Infomation</h3>
-                <div>ZIP Code: {this.props.data.zipcode}</div>
+                <div className="row">
+                  <div className="col-lg-9">
+                    Name: {this.props.data.name}
+                  </div>
+                  <div className="col-lg-3">
+                    Rate: $50/Day
+                  </div>
+                </div>
+                <div className='row'>
+                  <div className="col-lg-6">
+                    ZIP Code: {this.props.data.zipcode}
+                  </div>
+                  <div className="col-lg-3">
+                    V Boarding
+                  </div>
+                  <div className="col-lg-3">
+                    V Coming in
+                  </div>
+                </div>
                 <div>description: {this.props.data.description}</div>
-                <div>Review</div>
+                <div className="sitter-info-picture">
+                  <div className='row'>
+                    <div className="col-lg-4">
+                      <img className="media-object" height="120" width="160" src="https://images.pexels.com/photos/106399/pexels-photo-106399.jpeg?h=350&auto=compress&cs=tinysrgb" alt="" />
+                    </div>
+                    <div className="col-lg-4">
+                      <img className="media-object" height="120" width="160" src="https://assets3.thrillist.com/v1/image/2642224/size/tmg-article_tall.jpg" alt="" />
+                    </div>
+                    <div className="col-lg-4">
+                      <img className="media-object" height="120" width="160" src="https://assets3.thrillist.com/v1/image/2642231/size/tmg-article_tall.jpg" alt="" />
+                    </div>
+                  </div>
+                </div>
+                <div>
+                  Review:
+                </div>
+                <div className="sitter-info-reviewbox">
+                  <div>
+                    <SitterInfoViewReview />
+                    <SitterInfoViewReview />
+                    <SitterInfoViewReview />
+                    <SitterInfoViewReview />
+                    <SitterInfoViewReview />
+                  </div>
+                </div>
               </div>
             </div>
           </div>
@@ -31,7 +74,7 @@ class SittersInfo extends React.Component {
           <div className="col-lg-8">
           </div>
           <div className="col-lg-4">
-            <SendRequest />
+            <SendRequest data={this.props.data} />
           </div>
         </div>
       </div>

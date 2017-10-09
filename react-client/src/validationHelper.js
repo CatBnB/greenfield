1ef9d14eccd825c5c96b8896fe45e58d2851c0c5
Victor Wang <victor.wang@me.com>
2017-10-08 22:06:51 -0700

    add more detailed error message on form input

--- react-client/src/validationHelper.js
+++ react-client/src/validationHelper.js
@@ -3,22 +3,19 @@ import {getCoordinatesFromInput} from './geoHelper.js';
 const validateUserName = (input) => {
   let criteria = new RegExp(/^[A-Za-z0-9]+(?:[ _-][A-Za-z0-9]+)*$/);
   if (input && input.search(criteria) > -1) return input;
-  else return null;
-  // else throw new Error('invalid username');
+  else throw new Error('invalid username, must not contain special characters');
 };
 
 const validateEmail = (input) => {
   let criteria = new RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
   if (input && input.search(criteria) > -1) return input;
-  else return null;
-  // else throw new Error('invalid email address');
+  else throw new Error('invalid email address');
 };
 
 const validatePhone = (input) => {
   let criteria = new RegExp(/^[0-9]{10}$/);
   if (input && input.search(criteria) > -1) return input;
-  else return null;
-  // else throw new Error('invalid phone number');
+  else throw new Error('phone must be a valid 10 digit US phone number');
 };
 
 export const validateDates = (data) => {
@@ -34,11 +31,19 @@ export const validateDates = (data) => {
 export const validateInputs = (data) => {
   return new Promise(function(resolve, reject) {
     for (var item in data) {
-      if (item === 'name') data[item] = validateUserName(data[item]);
-      else if (item === 'email') data[item] = validateEmail(data[item]);
-      else if (item === 'phone') data[item] = validatePhone(data[item]);
-      console.log(item, data[item]);
-      if (data[item] === null) reject('All fields must be filled and data must be valid');
+
+      if (item === 'name') {
+        try { data[item] = validateUserName(data[item]); }
+        catch (e) { reject(e); }
+      } else if (item === 'email') {
+        try { data[item] = validateEmail(data[item]); }
+        catch (e) { reject(e); }
+      } else if (item === 'phone') {
+        try { data[item] = validatePhone(data[item]); }
+        catch (e) { reject(e); }
+      }
+
+      if (data[item] === null) reject(item + ' cannot be empty!');
     }
     resolve(data);
   });

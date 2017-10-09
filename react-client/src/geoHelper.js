15828a388a063276f56c51693f3e96caa72143fd
Victor Wang <victor.wang@me.com>
2017-10-09 11:18:53 -0700

    add fake reviews

--- react-client/src/geoHelper.js
+++ react-client/src/geoHelper.js
@@ -1,18 +1,18 @@
 import {get} from './ajaxHelper.js';
 
 // data rounded to 7 decimal places
-var formatGeoData = data => {
-  var coordinates = data.results[0].geometry.location;
+let formatGeoData = data => {
+  let coordinates = data.results[0].geometry.location;
   return [coordinates.lat, coordinates.lng];
 }
 
-var getAutocomplete = (input) => {
-  var endpoint = `https://maps.googleapis.com/maps/api/place/autocomplete/json?input=${input}&key=AIzaSyBcxddjCYsNoBYFD_52AGkJpFKw2lCjlOE`;
+let getAutocomplete = (input) => {
+  let endpoint = `https://maps.googleapis.com/maps/api/place/autocomplete/json?input=${input}&key=AIzaSyBcxddjCYsNoBYFD_52AGkJpFKw2lCjlOE`;
   return get(endpoint);
 }
 
 export const getCoordinates = (address) => {
-  var endpoint = `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=AIzaSyBcxddjCYsNoBYFD_52AGkJpFKw2lCjlOE`;
+  let endpoint = `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=AIzaSyBcxddjCYsNoBYFD_52AGkJpFKw2lCjlOE`;
   return get(endpoint).then(data => formatGeoData(data));
 }
 
